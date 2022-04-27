9772ee8c9d578aa115e35fa356d3207e4358919e
j-maestre <xemaiestacio@gmail.com>
2022-04-27 18:23:08 +0200

    add navigation function and emerge bugs

--- src/js/index.js
+++ src/js/index.js
@@ -1,47 +1,18 @@
-let menuEECont = 0;
-let currentEE = 0;
-const totalEE = 2;
 
 
-let menu = document.getElementById("menuBars");
-let eye = document.getElementById("ee_eye");
 
-eye.onclick = () =>{
-    ShowModal();
+let home = document.getElementById("home");
+let atari = document.getElementById("atari");
+
+
+home.onclick = () =>{
+    Navigate("home");
 }
-menu.onclick = () =>{
-    menuEECont++;
-    if(menuEECont==11){
-        ShowModal();
-    }
+atari.onclick = () =>{
+    Navigate("atari");
 }
 
 
-function ShowModal(text){
-    toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": false,
-        "progressBar": true,
-        "positionClass": "toast-bottom-right",
-        "preventDuplicates": false,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
-      }
-      toastr.success('Has encontrado un Easter Egg');
-      currentEE++;
-      ShowTotalEE();
-}
-
-
-
-function ShowTotalEE(){
-    alert("Total EE-> "+currentEE+"/"+totalEE);
+function Navigate(url){
+    document.getElementById("currentPage").src ="./pages/"+url+"/"+url+".html";
 }
---
+++ src/pages/home/home.js
@@ -0,0 +1,48 @@
+let menuEECont = 0;
+let currentEE = 0;
+const totalEE = 2;
+
+// let menu = document.getElementById("menuBars");
+let eye = document.getElementById("ee_eye");
+
+eye.onclick = () =>{
+    ShowModal("Has encontrado un Easter Egg");
+}
+// menu.onclick = () => {
+//     menuEECont++;
+//     if (menuEECont == 11) {
+//         ShowModal();
+//     }
+// }
+
+
+function ShowModal(text) {
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "newestOnTop": false,
+        "progressBar": true,
+        "positionClass": "toast-bottom-right",
+        "preventDuplicates": false,
+        "onclick": null,
+        "showDuration": "300",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+    console.log("show modal")
+    console.log(toastr);
+    toastr.success(text);
+    currentEE++;
+    ShowTotalEE();
+}
+
+
+
+function ShowTotalEE() {
+    alert("Total EE-> " + currentEE + "/" + totalEE);
+}
