50c64b9d4ce2bfceff59da0a2e9c6f970bbfe104
j-maestre <xemaiestacio@gmail.com>
2022-05-15 10:47:35 +0200

    add atari EE control

--- src/pages/atari/atari.js
+++ src/pages/atari/atari.js
@@ -1,4 +1,13 @@
 let ee_footer_bool = false;
+let currentEE = 0;
+if(localStorage.getItem("ee_footer")){
+    ee_footer_bool = localStorage.getItem("ee_footer");
+}
+
+if (localStorage.getItem("currentEE")){
+    currentEE = localStorage.getItem("currentEE");
+}
+
 let ee_footer = document.getElementById("ee_footer");
 
 
@@ -17,5 +26,8 @@ ee_footer.onclick = () => {
     if(!ee_footer_bool)ShowModal("Has encontrado un Easter Egg!");
     ee_footer_bool = false;
     ee_footer.style.display = "none";
+    currentEE++;
     localStorage.setItem("ee_footer",true);
+    localStorage.setItem("currentEE",currentEE);
+    ShowTotalEE();
 }
