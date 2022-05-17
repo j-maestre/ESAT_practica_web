b79d1da86d1e722a51841bb382d6ba88af3e53bc
j-maestre <xemaiestacio@gmail.com>
2022-05-17 12:23:58 +0200

    update contact ee

--- src/pages/contact/contact.js
+++ src/pages/contact/contact.js
@@ -1,10 +1,15 @@
+console.log("%c ", "background: black; padding: 1000000px;");
+
+
 document.getElementById("esat_ee").style.display = "none"
-function ESAT(){
-    document.getElementById("contact_container").style.display = "none";
-    document.getElementById("footer").style.display = "none";
-    document.getElementById("esat_ee").style.display = "block";
+function Codigo(code = ""){
+    if(code == "RESPONSIVE"){
+
+        document.getElementById("contact_container").style.display = "none";
+        document.getElementById("footer").style.display = "none";
+        document.getElementById("esat_ee").style.display = "block";
+        console.log("%c Ole los caracoles", "background: green; padding: 100px;");
+    }else{
+        console.log("%c Creo que se te ha olvidado algo...", "background: red; padding: 100px;");
+    }
 }
-console.log("uep");
-
-
-
