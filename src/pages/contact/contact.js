fc978f0c6500590487d86208177044410ce08b55
j-maestre <xemaiestacio@gmail.com>
2022-05-17 11:33:10 +0200

    add origami easter egg

---
+++ src/pages/contact/contact.js
@@ -0,0 +1,10 @@
+document.getElementById("esat_ee").style.display = "none"
+function ESAT(){
+    document.getElementById("contact_container").style.display = "none";
+    document.getElementById("footer").style.display = "none";
+    document.getElementById("esat_ee").style.display = "block";
+}
+console.log("uep");
+
+
+
--- src/pages/eggs/eggs.js
+++ src/pages/eggs/eggs.js
@@ -5,9 +5,5 @@ if(localStorage.getItem("currentEE")){
 }
 
 if (total != 6){
-    console.log("nanai de la chaina");
     document.getElementById("fourth_wall").style.display = "none";
-}else{
-    console.log("TODOS completados");
 }
-console.log("ola?")
